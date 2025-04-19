b3fa243fd67bd712e682ed8246d18ea5d6e189d6
Muhammad Badea <muhammad.badea@techno-island.com>
2025-04-19 15:47:32 +0200

    add: service collection extension for customer infrastructure and application

---
+++ src/Ecommerce.Customer.Application/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Customer.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Customer.Application;
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddInfrastructure();
+        return services;
+    }
+}
