322b197664ce6df7f054c9abdad5199fb160707f
Muhammad Badea <muhammad.badea@techno-island.com>
2025-04-21 15:54:15 +0200

    feat: value object impelemntation

--- src/Ecommerce.Base.Domain/Entity.cs
+++ src/Ecommerce.Base.Domain/Entity.cs
@@ -1,6 +1,28 @@
-﻿namespace Ecommerce.Base.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public class Entity
+namespace Ecommerce.Base.Domain;
+
+public abstract class Entity : IEquatable<Entity>, IEqualityComparer<Entity>
 {
     public Guid Id { get; init; }
+
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+        return Id.Equals(other.Id);
+    }
+
+    public bool Equals(Entity? x, Entity? y)
+    {
+        if (x is null && y is null) return true;
+        if (x is null || y is null) return false;
+        return x.Equals(y);
+    }
+
+    public int GetHashCode([DisallowNull] Entity obj)
+    {
+        return HashCode.Combine(Id, obj.Id);
+    }
 }
---
+++ src/Ecommerce.Customer.Domain/Address.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Base.Domain;
+
+namespace Ecommerce.Customer.Domain;
+public class Address(string city, string zipCode, string country) : ValueObject
+{
+    public string? Street { get; private init; }
+    public string City { get; private init; } = city;
+    public string? State { get; private init; }
+    public string ZipCode { get; private init; } = zipCode;
+    public string Country { get; private init; } = country;
+    public string? ApartmentNo { get; private init; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Street;
+        yield return City;
+        yield return State;
+        yield return ZipCode;
+        yield return Country;
+        yield return ApartmentNo;
+    }
+}
